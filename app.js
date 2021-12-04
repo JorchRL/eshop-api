17bab13755899d39d59374aeb4c9fb3b52b27601
Jorch <jorch.rl@gmail.com>
2021-12-04 14:25:49 -0600

    Setup initial express app

---
+++ app.js
@@ -0,0 +1,35 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+logger.info("Connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((e) => {
+    logger.error("error connecting to MongoDB", e.message);
+  });
+
+// Middleware
+app.use(express.static("build")); // serve the frontend
+app.use(express.json());
+app.use(cors);
+
+app.use(middleware.requestLogger);
+
+// ROUTES
+
+// todo
+
+// OTHERS
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
