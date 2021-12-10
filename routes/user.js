26d68b7a5c5f2e6757c28c24e52baa692c3d6d2a
Jorch <jorch.rl@gmail.com>
2021-12-10 17:22:04 -0600

    Add user route blank endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
+const User = require("../models/User");
+
+// Update an user by id
+router.put("/:id", (req, res, next) => []);
+
+// Delete an user by id
+router.delete("/:id", (req, res, next) => {});
+
+// Get user by id
+router.get("/find/:id", (req, res, next) => {});
+
+// Get all users
+router.get("/", (req, res, next) => {});
+
+// Get user stats
+router.get("/stats", (req, res, next) => {});
 
 module.exports = router;
